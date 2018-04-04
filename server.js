f9896229edab5a74e7283ccbd4725f940cdec358
pete <pete.dymarchuk@gmail.com>
2018-04-04 23:48:39 +0300

    test

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'))
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+var port = 8081, //process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
@@ -102,6 +102,5 @@ app.use(function(err, req, res, next){
 
 var server = app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
-setTimeout('server.close()', 200000);
 
 module.exports = app ;
