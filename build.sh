8cd4bfa533e1dbe00ae6f304511741ea5eb5f72a
pete <pete.dymarchuk@gmail.com>
2018-04-05 17:30:50 +0300

    test

--- build.sh
+++ build.sh
@@ -1,6 +1,1 @@
 #!/bin/bash
-(node server.js)
-(npm install websocket)
-(node chat-server.js)
-
-(PS1='\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$')
